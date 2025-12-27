569610bb249ec46f85a57cc596f9fde87558c82f
machdyne <philip@machdyne.com>
2025-12-27 11:12:14 +0100

    add ls10.h

---
+++ targets/ls10/ls10.h
@@ -0,0 +1,22 @@
+#ifndef _LS10_H
+#define _LS10_H
+
+#define ZW_GPIOA_PORT   GPIOC
+#define ZW_GPIOA        2
+#define ZW_GPIOB_PORT   GPIOC
+#define ZW_GPIOB        1
+#define ZW_GPIOC_PORT   GPIOD
+#define ZW_GPIOC        6
+#define ZW_GPIOD_PORT   GPIOD
+#define ZW_GPIOD        5
+
+#define ZW_GPIOE_PORT   GPIOC
+#define ZW_GPIOE        0
+#define ZW_GPIOF_PORT   GPIOC
+#define ZW_GPIOF        3  
+#define ZW_GPIOG_PORT   GPIOC
+#define ZW_GPIOG        4
+#define ZW_GPIOH_PORT   GPIOD    // LED
+#define ZW_GPIOH        4
+
+#endif
